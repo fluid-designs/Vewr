20645f2f168444a8cc7d5cff7a394294350970eb
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-07-22 16:57:06 -0700

    Dashboard skeleton complete.

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,9 +1,16 @@
 import React, { Fragment, Component } from "react";
 
+let userName = "Brutus";
+
 export default function Dashboard() {
   return (
     <Fragment>
-      <div className="Dashboard">Dashboard</div>
+      <h2>{`Welcome, ${userName}!!`}</h2>
+
+      <form>
+        <input />
+        <i className="fas fa-search fa-2x" />
+      </form>
     </Fragment>
   );
 }
