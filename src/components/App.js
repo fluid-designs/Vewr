39ae30a422d7df2e23bc476452f3273aaba213bc
Matt Stuhring <whereyouatmatt@gmail.com>
2019-07-22 17:10:12 -0700

    Implemented development routes for initial scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./navbar/navbar.js";
 import Login from "./login/login.js";
 import Dashboard from "./dashboard/dashboard.js";
+import Search from "./search/search.js";
+import Review from "./review/review.js";
 import AboutUs from "./about-us/about-us.js";
 
 export default class App extends Component {
@@ -22,6 +24,8 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/dashboard" component={Dashboard} />
+            <Route path="/search" component={Search} />
+            <Route path="/review" component={Review} />
             <Route path="/about-us" component={AboutUs} />
           </Switch>
         </Fragment>
