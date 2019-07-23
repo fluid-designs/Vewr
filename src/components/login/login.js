a1a23e0b6a9e8a0e0687b2f47909603d384070d3
Matt Stuhring <whereyouatmatt@gmail.com>
2019-07-22 18:02:31 -0700

    Scaffold login component complete

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,52 @@
 import React, { Fragment, Component } from "react";
 
-export default function Login() {
-  return (
-    <Fragment>
-      <div className="Login">Login</div>
-    </Fragment>
-  );
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Username: ", this.state.username);
+
+    this.props.history.push("/dashboard");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="login">
+          <h1>Login</h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                placeholder="Enter your username"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </Fragment>
+    );
+  }
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <Fragment>
-      <nav className="Navbar">
+      <nav className="navbar">
         <Link to="/">
           <h1>VEWR</h1>
         </Link>
