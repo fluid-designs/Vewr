2aef165aa8030111c35ec0b583dc31f83bfdecd6
MELFI PEREZ <perezmelfi@outlook.com>
2019-07-23 15:13:11 -0700

    cleaning up linter errors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-"use-strict";
+'use-strict';
 
 // Dependencies
-const express = require("express");
-const superagent = require("superagent");
-const pg = require("pg");
-const cors = require("cors");
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
 
-require("dotenv").config();
+require('dotenv').config();
 
 // Application setup
 const app = express();
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 5000;
 // DB setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on("error", err => console.error(err));
+client.on('error', err => console.error(err));
 
 // API Routes
 app.get('/search', getMovieAPIResults);
-app.get("/movies", getMovieAPIResults);
+app.get('/movies', getMovieAPIResults);
 
 // Ensures server is listening for requests
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -28,18 +28,18 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 // Error Handling
 function handleError(err, result) {
   console.error(err);
-  if (result) result.status(500).send("Something went wrong internal error");
+  if (result) result.status(500).send('Something went wrong internal error');
 }
 
 // Movies Constructor
 function Movies(movie) {
-  this.tableName = "movies";
+  this.tableName = 'movies';
   this.movie_id = movie.id;
   this.title = movie.original_title;
   this.synopsis = movie.overview;
   this.released_on = movie.released_date;
   this.image_url =
-    `https://image.tmdb.org/t/p/w500${movie.poster_path}` || "Image";
+    `https://image.tmdb.org/t/p/w500${movie.poster_path}` || 'Image';
 }
 
 function getMovieAPIResults(request, response) {
@@ -58,17 +58,23 @@ function getMovieAPIResults(request, response) {
 function urlBuilder(request) {
   const searchTarget = request.query.data;
   const searchType = request.query.url;
-  let url = "";
+  let url = '';
   switch (searchType) {
-    case "movies":
-      url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchTarget}`;
-      break;
-    case "search":
-      url = `https://api.themoviedb.org/3/movie/${searchTarget}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
-      break;
-    default:
-      // TODO: Is this default correct?
-      url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchTarget}`;
+  case 'movies':
+    url = `https://api.themoviedb.org/3/search/movie?api_key=${
+      process.env.MOVIE_API_KEY
+    }&query=${searchTarget}`;
+    break;
+  case 'search':
+    url = `https://api.themoviedb.org/3/movie/${searchTarget}?api_key=${
+      process.env.MOVIE_API_KEY
+    }&language=en-US`;
+    break;
+  default:
+    // TODO: Is this default correct?
+    url = `https://api.themoviedb.org/3/search/movie?api_key=${
+      process.env.MOVIE_API_KEY
+    }&query=${searchTarget}`;
   }
   return url;
 }
