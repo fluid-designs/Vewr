f5d6f435eed5fcc46258e57462e30b12e0337a7a
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-07-24 11:59:51 -0700

    Styling overhaul.

--- src/components/about-us/about-us.js
+++ src/components/about-us/about-us.js
@@ -5,8 +5,8 @@ export default function AboutUs() {
   return (
     <Fragment>
       <div id="AboutUs" class="component-container">
-        <h1>Team Fluid</h1>
-        
+        <h1>Fluid Design</h1>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/janehur.svg" />
@@ -14,17 +14,17 @@ export default function AboutUs() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/brandonhurrington.svg" />
-        
+
         <h2>Brandon Hurrington</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/melfiperez.svg" />
-        
+
         <h2>Melfi Perez</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/mattstuhring.svg" />
-        
+
         <h2>Matt Stuhring</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
       </div>
