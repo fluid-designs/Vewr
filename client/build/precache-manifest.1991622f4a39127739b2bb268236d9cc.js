ed57b0995f053b697ab69f2c4de399059a036d45
MELFI PEREZ <perezmelfi@outlook.com>
2019-07-24 16:29:16 -0700

    more debugging

---
+++ client/build/precache-manifest.1991622f4a39127739b2bb268236d9cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8396fd9c0fbc9db6d716659c64e9eb13",
+    "url": "/fluid-designs/Vewr/index.html"
+  },
+  {
+    "revision": "ea7cb9370266ed04aa26",
+    "url": "/fluid-designs/Vewr/static/css/main.b473e220.chunk.css"
+  },
+  {
+    "revision": "ced754503bfa219d27cb",
+    "url": "/fluid-designs/Vewr/static/js/2.3fd8776d.chunk.js"
+  },
+  {
+    "revision": "ea7cb9370266ed04aa26",
+    "url": "/fluid-designs/Vewr/static/js/main.eca75451.chunk.js"
+  },
+  {
+    "revision": "46b73994b68f8df14851",
+    "url": "/fluid-designs/Vewr/static/js/runtime~main.1fab7456.js"
+  }
+]);
