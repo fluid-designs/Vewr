09850222808a817df5259dffd6b1aee72442a874
MELFI PEREZ <perezmelfi@outlook.com>
2019-07-24 15:53:16 -0700

    testing deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -29,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 // Serve static files from the React app
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // API Routes
 app.get('/search', getMovieAPIResults);
@@ -42,7 +43,7 @@ app.get('/reviews', getUserReviews);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile('client/build/index.html');
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // Ensures server is listening for requests
