beccdd106b6a02ae82ced1e62d6be401be545c18
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-07-24 18:02:31 -0700

    Some changes.

--- src/components/about-us/about-us.js
+++ src/components/about-us/about-us.js
@@ -1,31 +1,34 @@
 import React, { Fragment, Component } from "react";
+import { Link } from "react-router-dom";
+
 
 
 export default function AboutUs() {
   return (
     <Fragment>
-      <div id="AboutUs" class="component-container">
+      <div id="AboutUs" className="component-container">
         <h1>Fluid Design</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Fresh as the spring rain. Churning and thrashing as rapids. Swift as the stream. Patient as glacial runoff. Cool as the winter snow.  Amorphous and changeable throughout. We are Fluid Design. #we'rethesmartgroup</p>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/janehur.svg" />
         <h2>Jane Hur</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <p>A spellcaster from the Land of the Winds that trained with the Great Lakes Ancients, she still practices the old ways, hexing code into any shape imaginable and likely the one that most amuses her. Rightly feared by the Council of Guardians for the power of her code magic, she can bend any code base to her will.</p>
+        <a href="https://github.com/janiekyu"><i className="fab fa-github">Github</i></a>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/brandonhurrington.svg" />
-
         <h2>Brandon Hurrington</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="https://github.com/Kai-ros"><i className="fab fa-github">Github</i></a>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/melfiperez.svg" />
-
         <h2>Melfi Perez</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="https://github.com/mattstuhring"><i className="fab fa-github">Github</i></a>
         <hr />
         <img src="https://avatars.dicebear.com/v2/bottts/mattstuhring.svg" />
-
         <h2>Matt Stuhring</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="https://github.com/perezm27"><i className="fab fa-github">Github</i></a>
       </div>
     </Fragment>
   );
